d3552321e5f7829a14038c815a366e629ea75da8
abdeljalil <89589355+Abdeljalil-22@users.noreply.github.com>
2024-02-09 16:18:51 +0000

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 // ChartComponent
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'Chart', component: ChartComponent }
+  
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
